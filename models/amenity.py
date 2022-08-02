ff3573c81da8ddb0e10d4429b8eadf6630dff300
lola-source <adagaladolores@gmail.com>
2022-08-02 20:35:48 +0000

    airbnb

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+
+lds class Amenity"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Representation of Amenity """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
