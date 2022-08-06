607e8dc4ec0dc4b111f48f0e8a297b48e4fda539
lola-source <99507617+lola-source@users.noreply.github.com>
2022-08-07 01:54:08 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import environ as env
-import models
+"""
+State Class from Models Module
+"""
+
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        __tablename__: table name
-        name: input name
-        cities: relation to cities table
-    """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
+class State(BaseModel):
+    """State class handles all application states"""
 
-    if env.get('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            """get all cities with the current state id
-            from filestorage
-            """
-            l = [
-                v for k, v in models.storage.all(models.City).items()
-                if v.state_id == self.id
-            ]
-            return (l)
+    name = ''
+
+    def __init__(self, *args, **kwargs):
+        """instantiates a new state"""
+        super().__init__(self, *args, **kwargs)
