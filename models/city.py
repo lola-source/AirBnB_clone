1f442fdfd2b602d99b6d03f684760f55d69d206a
lola-source <99507617+lola-source@users.noreply.github.com>
2022-08-07 01:51:06 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""This is the city class"""
-from models.base_model import BaseModel, Base
-from models.state import State
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+"""
+City Class from Models Module
+"""
+
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        __tablename__: name of the table represented
-        state_id: The state id
-        name: input name
-    """
+class City(BaseModel):
+    """City class handles all application cities"""
 
-    __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey(State.id), nullable=False)
-    name = Column(String(128), nullable=False)
-    places = relationship("Place", cascade="all, delete", backref="city")
+    state_id = ''
+    name = ''
+
+    def __init__(self, *args, **kwargs):
+        """instantiates a new city"""
+        super().__init__(self, *args, **kwargs)
