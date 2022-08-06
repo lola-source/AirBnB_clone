4de2af30e5b269705ddbcc1c7d2e0806583401fc
lola-source <adagaladolores@gmail.com>
2022-08-06 21:46:03 +0000

    strg

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,20 @@
+#!/usr/bin/python3
+"""This is the amenity class"""
 
-lds class Amenity"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.place import place_amenity
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-
-class Amenity(BaseModel):
-    """Representation of Amenity """
-    name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+    name: input name
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship(
+        "Place",
+        secondary=place_amenity,
+        backref="amenity"
+)
