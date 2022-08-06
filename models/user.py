df107dd21864bbc4ace1a3e9f82099586fc2869d
lola-source <99507617+lola-source@users.noreply.github.com>
2022-08-07 01:54:59 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+"""
+User Class from Models Module
+"""
+
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        __tablename__: Table name
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", cascade="all, delete", backref="user")
-    reviews = relationship("Review", cascade="all, delete", backref="user")
+class User(BaseModel):
+    """User class handles all application users"""
+
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
+
+    def __init__(self, *args, **kwargs):
+        """instantiates a new user"""
+        super().__init__(self, *args, **kwargs)
